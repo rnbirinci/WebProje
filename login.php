71468710239660d4a918778c755c1aad5db5c634
rnbirinci <rananil05@gmail.com>
2025-05-23 22:49:20 +0300

    ondokuzuncu commit

--- login.php
+++ login.php
@@ -1,49 +1,12 @@
 <?php
 
-$user = $_POST['user'] ?? '';
-$pass = $_POST['pass'] ?? '';
-$validUser = 'rana.birinci@ogr.sakarya.edu.tr';
-$validPass = 'g231210552';
-if ($user === $validUser && $pass === $validPass) {
+$kullanici = $_GET['kullanici'] ?? '';
+$sifre = $_GET['sifre'] ?? '';
+$validKullanici = 'rana.birinci@ogr.sakarya.edu.tr';
+$validSifre = 'g231210552';
+if ($kullanici === $validKullanici && $sifre === $validSifre) {
   echo "<h1>Hoşgeldiniz, g231210552!</h1>";
 } else {
   header('Location: login.html');
 }
-
-
-//" . htmlspecialchars(substr($validPass,1)) .
-/* function is_valid_email($email) {
-    return filter_var($email, FILTER_VALIDATE_EMAIL);
-}
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'] ?? '';
-    $password = $_POST['password'] ?? '';
-
-    // Boş alan kontrolü
-    if (empty($username) || empty($password)) {
-        header("Location: login.html");
-        exit();
-    }
-
-    // E-posta kontrolü
-    if (!is_valid_email($username)) {
-        header("Location: login.html");
-        exit();
-    }
-
-    // Kullanıcı adı ve şifre doğrulama
-    $expectedDomain = '@sakarya.edu.tr';
-    $studentId = strstr($username, '@', true); // 'b1812100001'
-
-    if (str_ends_with($username, $expectedDomain) && $password === $studentId) {
-        echo "Hoşgeldiniz $password";
-    } else {
-        header("Location: login.html");
-        exit();
-    }
-} else {
-    header("Location: login.html");
-    exit();
-} */
 ?>
