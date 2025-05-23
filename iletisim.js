05183bacdf38470a0cb2ae1d096d2fb32a90a7f7
rnbirinci <rananil05@gmail.com>
2025-05-23 22:06:15 +0300

    onsekizinci commit

--- iletisim.js
+++ iletisim.js
@@ -1,21 +1,21 @@
 Vue.createApp({
     data(){ return {
-      form: { email:'', phone:'', message:'' },
+      form: { email:'', telefon:'', mesaj:'' },
       submitted: false
     }},
     methods: {
       validate() {
         let ok = true;
         if (!/\S+@\S+\.\S+/.test(this.form.email)) ok = false;        
-        if (!/^\d+$/.test(this.form.phone)) ok = false;
-        if (!this.form.message) ok = false;
+        if (!/^\d+$/.test(this.form.telefon)) ok = false;
+        if (!this.form.mesaj) ok = false;
         alert(ok ? 'Valid!' : 'Hatalı giriş!');
       },
       handleSubmit() {
         this.submitted = true;
       },
       handleReset() {
-        this.form = { email:'', phone:'', message:'' };
+        this.form = { email:'', telefon:'', mesaj:'' };
         this.submitted = false;
       }
     }
